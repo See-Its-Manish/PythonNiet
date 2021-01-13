bcf0f4b942fec05b962001229b063d158eb60e26
See-Its-Manish <seeitsmanish@gmail.com>
2021-01-13 23:48:47 +0530

    Modified commit

--- GreatestOfThree.py
+++ GreatestOfThree.py
@@ -6,11 +6,9 @@ n1 = int(n1)
 n2 = int(n2)
 n3 = int(n3)
 
-if n1 > n2 and n1 > n3:
+if n1 >= n2 and n1 >= n3:
     print("Greatest of three is ", n1)
-elif n2 > n1 and n2 > n1:
+elif n2 >= n1 and n2 >= n3:
     print("Greatest of three is ", n2)
-elif n3 > n1 and n3 > n2:
+else:
     print("Greatest of three is ", n3)
-elif n1 == n2 and n2 == n3:
-    print("All are same equal to", n1)
