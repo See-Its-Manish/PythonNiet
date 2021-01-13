6eb38dba06a3f78889bb6ffab174e5299f350130
See-Its-Manish <seeitsmanish@gmail.com>
2021-01-13 19:16:54 +0530

    Comment Added in isArmstrong.py

--- isArmstrong.py
+++ isArmstrong.py
@@ -4,7 +4,7 @@ n = input("Enter a number: ")
 n = int(n)
 
 toComp = int(n)
-numOfDigit = int(math.log10(n))+1
+numOfDigit = int(math.log10(n))+1  # This function will find number of digits in given number
 isOver = 0
 isOver = int(isOver)
 sumOfNum = 0
